6e53df774d7319fb4311f331c5bc52169f9eb754
trikytrukos <miguelmartinezsantos2003@gmail.com>
2023-12-01 13:44:39 +0100

    ofertas full responsive!!

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
 document.getElementById('menu-toggle').addEventListener('click', function () {
     document.getElementById('menu').classList.toggle('hidden');
 });
-var mySwiper = new Swiper('.swiper-container', {
-    // Opciones de configuración
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
