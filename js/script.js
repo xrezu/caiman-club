a3edee21115549bf97b330f7774e4d59b93b8a3c
trikytrukos <miguelmartinezsantos2003@gmail.com>
2023-12-08 19:16:53 +0100

    memoria y carrusel de eventos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 document.getElementById('menu-toggle').addEventListener('click', function () {
     document.getElementById('menu').classList.toggle('hidden');
 });
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30, // Espacio entre slides, ajustar según necesidad
+    loop: true, // Hace que el carrusel sea continuo
+
+    // Opciones de paginación
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Opciones de navegación
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Opciones de la barra de desplazamiento
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
