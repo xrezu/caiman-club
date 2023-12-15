03e18fb8e96907af04262aa68ad377327291ebd5
trikytrukos <miguelmartinezsantos2003@gmail.com>
2023-12-15 16:15:15 +0100

    eventos en la seccion de eventos

--- js/script.js
+++ js/script.js
@@ -1,27 +1,34 @@
-document.getElementById('menu-toggle').addEventListener('click', function () {
-    document.getElementById('menu').classList.toggle('hidden');
-});
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('menu-toggle').addEventListener('click', function () {
+        document.getElementById('menu').classList.toggle('hidden');
+    });
 
 
-var mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30, // Espacio entre slides, ajustar según necesidad
-    loop: true, // Hace que el carrusel sea continuo
+    var mySwiper = new Swiper('.swiper-container', {
+        // Configuración básica
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
 
-    // Opciones de paginación
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-    // Opciones de navegación
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // Opciones de la barra de desplazamiento
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+        // Configuraciones de respuesta
+        breakpoints: {
+            // cuando la anchura es >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // cuando la anchura es >= 640px
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // cuando la anchura es >= 1024px
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+        }
+    });
 });
